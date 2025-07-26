a9f8879d2ba9743f8d0e50d370722ebe18bf7670
Adnane Hamid <adnane.hamid.20@gmail.com>
2025-07-26 05:18:17 +0200

    Update restrict.ts

--- src/restrict.ts
+++ src/restrict.ts
@@ -6,22 +6,28 @@ export type Permission = 'r' | 'w' | 'rw' | 'none';
 
 const META_KEY = Symbol('store:permission');
 
-export function Restrict(permission: Permission = 'none'): PropertyDecorator {
-  if (!['r', 'w', 'rw', 'none'].includes(permission)) {
-    throw new InvalidPermissionError(`Invalid permission: ${permission}`);
-  }
+export function Restrict<
+  K extends string,
+  V extends StoreValue
+>(permission: Permission = 'none') {
+  return function <
+    T extends Store & Record<K, V>
+  >(target: T, propertyKey: K): void {
 
-  return (target: object, propertyKey: string | symbol): void => {
+    if (!['r', 'w', 'rw', 'none'].includes(permission)) {
+      throw new InvalidPermissionError(`Invalid permission: ${permission}`);
+    }
+
     Reflect.defineMetadata(META_KEY, permission, target, propertyKey);
 
     Object.defineProperty(target, propertyKey, {
       configurable: true,
       enumerable: true,
       get(this: Store) {
-        return this.getField(propertyKey as string);
+        return this.getField(propertyKey);
       },
-      set(this: Store, value: StoreValue) {
-        this.setField(propertyKey as string, value);
+      set(this: Store, value: V) {
+        this.setField(propertyKey, value);
       }
     });
   };
